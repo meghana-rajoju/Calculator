66528a62564382be0c43d2b1a078c6d127c9c977
meghana-rajoju <meghanarajoju20@gmail.com>
2025-01-29 17:59:10 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Simple Calculator");
+        System.out.println("Select an operation:");
+        System.out.println("1. Add");
+        System.out.println("2. Subtract");
+        System.out.println("3. Multiply");
+        System.out.println("4. Divide");
+        
+        int choice = scanner.nextInt();
+        
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        
+        double result = 0;
+        boolean validOperation = true;
+
+        switch (choice) {
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+                if (num2 == 0) {
+                    System.out.println("Error! Division by zero is not allowed.");
+                    validOperation = false;
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                System.out.println("Invalid choice!");
+                validOperation = false;
+                break;
+        }
+
+        if (validOperation) {
+            System.out.println("The result is: " + result);
+        }
+
+        scanner.close();
+    }
+}
